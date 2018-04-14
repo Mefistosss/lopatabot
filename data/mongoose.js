08a6b9e765fc76fd4136ed747e3964103856791c
Mefistosss <Mefistosss@ukr.net>
2018-04-14 17:50:57 +0300

    env

--- data/mongoose.js
+++ data/mongoose.js
@@ -6,42 +6,52 @@ var user = null;
 var password = null;
 
 var mongoUrl = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL || process.env.MONGOURL;
-var name = process.env.MONGONAME || config.get('mongoName');
+var name = process.env.MONGO_NAME || config.get('mongoName');
 
 var options = {
     "keepAlive": true
 };
 
-if (mongoUrl == null && process.env.DATABASE_SERVICE_NAME) {
-    var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-        mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-        mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-        mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-        mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-        mongoUser = process.env[mongoServiceName + '_USER'];
+// if (mongoUrl == null && process.env.DATABASE_SERVICE_NAME) {
+//     var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+//         mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+//         mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+//         mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+//         mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+//         mongoUser = process.env[mongoServiceName + '_USER'];
 
-    if (mongoHost && mongoPort && mongoDatabase) {
-        // mongoURLLabel = mongoURL = 'mongodb://';
-        if (mongoUser && mongoPassword) {
-            // mongoURL += mongoUser + ':' + mongoPassword + '@';
+//     if (mongoHost && mongoPort && mongoDatabase) {
+//         // mongoURLLabel = mongoURL = 'mongodb://';
+//         if (mongoUser && mongoPassword) {
+//             // mongoURL += mongoUser + ':' + mongoPassword + '@';
 
-            options.user = mongoUser;
-            options.pass = mongoPassword;
+//             options.user = mongoUser;
+//             options.pass = mongoPassword;
 
-            console.log('USER PASS: true');
-        }
-        // Provide UI label that excludes user id and pw
-        // mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-        // mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
-        mongoURL = mongoHost + ':' +  mongoPort;
-        name = mongoDatabase;
-    }
-}
+//             console.log('USER PASS: true');
+//         }
+//         // Provide UI label that excludes user id and pw
+//         // mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+//         // mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+//         mongoURL = mongoHost + ':' +  mongoPort;
+//         name = mongoDatabase;
+//     }
+// }
+
+options.user = process.env.MONGO_USER;
+options.pass = process.env.MONGO_PASS;
+
+mongoHost = process.env.MONGO_HOST;
+mongoPort = process.env.MONGO_PORT;
+
+mongoURL = mongoHost + ':' +  mongoPort;
 
 if (mongoUrl == null) {
     mongoUrl = config.get('mongoUrl');
 }
 
+console.log(process.env);
+console.dir(process.env);
 console.log('MONGOURL', 'mongodb://' + mongoUrl + '/' + name);
 
 mongoose.connect('mongodb://' + mongoUrl + '/' + name, options);
