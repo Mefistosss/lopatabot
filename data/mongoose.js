6bbc09f9c50d32930dc014e34fa12d76443accf5
Mefistosss <Mefistosss@ukr.net>
2018-04-15 13:13:25 +0300

    test

--- data/mongoose.js
+++ data/mongoose.js
@@ -112,6 +112,8 @@ if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
   }
 }
 
+mongoUrl = '127.0.0.1:27017/' + process.env.MONGODB_DATABASE;
+
 console.log('MONGOURL', mongoUrl);
 mongoose.connect(mongoUrl, function (err) {
     if (err) {
