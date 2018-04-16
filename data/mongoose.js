d0ac73a754328a187f77799f26f64600663b0d0e
Mefistosss <Mefistosss@ukr.net>
2018-04-16 22:45:22 +0300

    test

--- data/mongoose.js
+++ data/mongoose.js
@@ -112,10 +112,10 @@ if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
   }
 }
 
-mongoUrl = 'mongodb://' + mongoURL;
+// mongoUrl = 'mongodb://' + mongoURL;
 
-console.log('MONGOURL', mongoUrl);
-mongoose.connect(mongoUrl, function (err) {
+console.log('MONGOURL', mongoURL);
+mongoose.connect(mongoURL, function (err) {
     if (err) {
         console.log('connect error');
         console.log(err);
