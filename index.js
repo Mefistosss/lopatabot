aedecfa6c1d786d3b4a28ab6f4aacf41f4cdff89
Mefistosss <Mefistosss@ukr.net>
2018-04-22 08:20:39 +0300

    healthcheck

--- index.js
+++ index.js
@@ -13,117 +13,120 @@ process.on('unhandledRejection', (reason, p) => {
     // application specific logging, throwing an error, or other logic here
 });
 
-db(function () {
-    var PORT = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || config.get('port');
-    var TOKEN = process.env.TOKEN;
-    var URL = process.env.APP_URL || process.env.OPENSHIFT_NODEJS_IP;
-
-    var bot = new TelegramBot(TOKEN, {
-        webHook: {
-            port: PORT,
-            autoOpen: false
-        }
-    });
-
-    bot.openWebHook();
-    bot.setWebHook(URL + '/bot' + TOKEN);
+var PORT = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || config.get('port');
+var TOKEN = process.env.TOKEN;
+var URL = process.env.APP_URL || process.env.OPENSHIFT_NODEJS_IP;
 
 
-    bot.on('inline_query', function (query) {
-        var results = [];
-
-        if (query.query.trim() !== '') {
-            var l = "лопата";
-            var s = "сарказм";
-            var m = getMessage(query.query, l);
-
-            results = [
-                {
-                    id: query.id + '-3',
-                    type: 'article',
-                    title: '</' + s + '>',
-                    description: 'Просто обернуть',
-                    input_message_content: {
-                        message_text: getMessage(query.query, s),
-                        parse_mode:  'Markdown'
-                    }
-                },
-                {
-                    id: query.id + '-2',
-                    type: 'article',
-                    title: '#хохма </' + l + '>',
-                    description: 'Обернуть и добавить хештег #хохма',
-                    input_message_content: {
-                        message_text: "#хохма \n" + m,
-                        parse_mode:  'Markdown'
-                    }
-                },
-                {
-                    id: query.id + '-1',
-                    type: 'article',
-                    title: '</' + l  + '>',
-                    description: 'Просто обернуть',
-                    input_message_content: {
-                        message_text: m,
-                        parse_mode:  'Markdown'
-                    }
-                }
-            ];
-        }
-
-        bot.answerInlineQuery(query.id, results, { cash_time: 0 });
-    });
-
-    bot.onText(/\/start\b/, function (msg) {
-        bot.sendMessage(msg.chat.id, config.get('phrases.welcome'));
-    });
-
-    bot.onText(/\/help\b/, function (msg) {
-        var message = config.get('phrases.welcome');
-        message += "\n\n";
-        message += config.get('phrases.help');
-        bot.sendMessage(msg.chat.id, message);
-    });
-
-    bot.onText(/\/anekdot/, function (msg) {
-        anekdot(function (data) {
-            bot.sendMessage(msg.chat.id, data);
-        });
-    });
-
-    bot.onText(/\/version/, function (msg) {
-        bot.sendMessage(msg.chat.id, version);
-    });
-
-    var groups = new Groups(function (ids) {
-        if (ids.length) {
-            anekdot(function (data) {
-                var message = makeChatMessage(data, 'morning');
-                ids.forEach(function (id) {
-                    bot.sendMessage(id, message);
-                });
-            });
-        }
-    });
-
-    bot.onText(/\/startnotices/, function (msg) {
-        groups.add(msg.chat, function (err, message) {
-            if (!err) {
-                bot.sendMessage(msg.chat.id, message);
-            }
-        });
-    });
-
-    bot.onText(/\/stopnotices/, function (msg) {
-        groups.remove(msg.chat.id, function (err, message) {
-            if (!err) {
-                bot.sendMessage(msg.chat.id, message);
-            }
-        });
-    });
-
-    groups.startJob();
-
-    console.log('URL', URL);
-    console.log('PORT', PORT);
+var bot = new TelegramBot(TOKEN, {
+    webHook: {
+        port: PORT,
+        autoOpen: false,
+        healthEndpoint: "/pagecount"
+    }
 });
+
+bot.openWebHook();
+bot.setWebHook(URL + '/bot' + TOKEN);
+
+bot.on('inline_query', function (query) {
+    var results = [];
+
+    if (query.query.trim() !== '') {
+        var l = "лопата";
+        var s = "сарказм";
+        var m = getMessage(query.query, l);
+
+        results = [
+            {
+                id: query.id + '-3',
+                type: 'article',
+                title: '</' + s + '>',
+                description: 'Просто обернуть',
+                input_message_content: {
+                    message_text: getMessage(query.query, s),
+                    parse_mode:  'Markdown'
+                }
+            },
+            {
+                id: query.id + '-2',
+                type: 'article',
+                title: '#хохма </' + l + '>',
+                description: 'Обернуть и добавить хештег #хохма',
+                input_message_content: {
+                    message_text: "#хохма \n" + m,
+                    parse_mode:  'Markdown'
+                }
+            },
+            {
+                id: query.id + '-1',
+                type: 'article',
+                title: '</' + l  + '>',
+                description: 'Просто обернуть',
+                input_message_content: {
+                    message_text: m,
+                    parse_mode:  'Markdown'
+                }
+            }
+        ];
+    }
+
+    bot.answerInlineQuery(query.id, results, { cash_time: 0 });
+});
+
+bot.onText(/\/start\b/, function (msg) {
+    bot.sendMessage(msg.chat.id, config.get('phrases.welcome'));
+});
+
+bot.onText(/\/help\b/, function (msg) {
+    var message = config.get('phrases.welcome');
+    message += "\n\n";
+    message += config.get('phrases.help');
+    bot.sendMessage(msg.chat.id, message);
+});
+
+bot.onText(/\/anekdot/, function (msg) {
+    anekdot(function (data) {
+        bot.sendMessage(msg.chat.id, data);
+    });
+});
+
+bot.onText(/\/version/, function (msg) {
+    bot.sendMessage(msg.chat.id, version);
+});
+
+var groups = new Groups(function (ids) {
+    if (ids.length) {
+        anekdot(function (data) {
+            var message = makeChatMessage(data, 'morning');
+            ids.forEach(function (id) {
+                bot.sendMessage(id, message);
+            });
+        });
+    }
+});
+
+bot.onText(/\/startnotices/, function (msg) {
+    groups.add(msg.chat, function (err, message) {
+        if (!err) {
+            bot.sendMessage(msg.chat.id, message);
+        }
+    });
+});
+
+bot.onText(/\/stopnotices/, function (msg) {
+    groups.remove(msg.chat.id, function (err, message) {
+        if (!err) {
+            bot.sendMessage(msg.chat.id, message);
+        }
+    });
+});
+
+db(function () {
+    groups.startJob();
+});
+
+console.log('URL', URL);
+console.log('PORT', PORT);
+
+
